2a63f91c6c894e743a2828db8147e72515328667
fartbagxp <fartbagxp@gmail.com>
2018-09-16 03:27:12 -0400

    Refactoring validation (#13)
    * Refactoring validation and breaking it out into another section of code
    
    * Updating location of a test package
    
    * Adding circleci build status to README

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![CircleCI](https://circleci.com/gh/fartbagxp/monitor-dslogon.svg?style=svg)](https://circleci.com/gh/fartbagxp/monitor-dslogon)
 [![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
 [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](http://makeapullrequest.com)
 [![MIT License](https://img.shields.io/github/license/dawnlabs/carbon.svg)](https://github.com/dawnlabs/carbon/blob/master/LICENSE)
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const performance = require('perf_hooks').performance;
 const request = require('superagent');
 
 const config = require('./config');
+const validation = require('./validate');
 const statuspageio = require('./statuspage');
 
 /**
@@ -18,32 +19,6 @@ async function screenshot(page, filename) {
     return;
   }
   await page.screenshot({ path: `debug/${filename}` });
-}
-
-/**
- * The thinking here is that if we can validate the HTML on some level,
- * we can ensure that the site is up provided the HTML.
- */
-function validateHtml(html) {
-  if (html == null || html === '') {
-    return 'No HTML found, could not validate HTML.';
-  }
-
-  const SEARCH_TERMS = [
-    'currently logged on',
-    'DS Logon',
-    'Premium',
-    'DS Logon Account Level'
-  ];
-
-  // look for particular search terms to ensure login is successful.
-  for (const term in SEARCH_TERMS) {
-    if (html.indexOf(term) === -1) {
-      return `Could not verify content due to missing key term:${term} in HTML.`;
-    }
-  }
-
-  return null;
 }
 
 /**
@@ -239,7 +214,7 @@ const retry = (fn, ms = 1000, maxRetries = 5) =>
   );
 
   // validate the HTML and notify the monitoring system
-  let errorText = validateHtml(bodyHTML);
+  let errorText = validation.validateHtml(bodyHTML);
   if (debug) {
     notifySlack(slack, errorText, timeInMs / 1000);
   }
